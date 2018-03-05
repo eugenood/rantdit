ba59ac887167f4c83b1ec28d8a217c3e752effa1
Eugene Lim <nexolute@gmail.com>
2018-03-05 18:17:27 +0800

    Change RantItem to include Voter

--- views/components/RantItem.jsx
+++ views/components/RantItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Voter from './Voter';
 
 function RantItem(props) {
   return (
     <div>
       <p>{props.rant.message}</p>
       <p>posted by {props.rant.user}</p>
+      <Voter rant={props.rant} />
     </div>
   )
 }
