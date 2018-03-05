366cc1d37fbc1525a47ff898ac1d88f1b645db03
Eugene Lim <nexolute@gmail.com>
2018-03-05 17:51:58 +0800

    Add RantList
    RantList displays a list of rants by mapping each rant to a RantItem.

---
+++ views/components/RantList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RantItem from './RantItem';
+
+function RantList(props) {
+  const rantItems = props.rants.map((rant) => {
+    return (
+      <RantItem
+        key={rant.id}
+        rant={rant}
+        onUpvote={props.onUpvote}
+        onDownvote={props.onDownvote}
+      />
+    );
+  });
+
+  return <div>{rantItems}</div>;
+}
+
+RantList.propTypes = {
+  rants: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onUpvote: PropTypes.func,
+  onDownvote: PropTypes.func
+}
+
+export default RantList;
