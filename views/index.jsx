39e0e2f45fd9beb1ac293a697faffc8c8ac6db9a
Eugene Lim <nexolute@gmail.com>
2018-03-05 17:29:06 +0800

    Add index for views
    index.html and index.js is the entry point for the application.

---
+++ views/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FrontPage from './pages/FrontPage';
+
+function App() {
+  return <FrontPage />;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
